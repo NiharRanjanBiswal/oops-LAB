e4ac0d4e9d1df9e5c8a48c935c0181109b41d415
Nihar Ranjan Biswal <95560017+NiharRanjanBiswal@users.noreply.github.com>
2022-11-01 23:36:51 +0530

    Update Q1..class complex which stores real and imaginary part of a complex number.cpp

--- Q1..class complex which stores real and imaginary part of a complex number.cpp
+++ Q1..class complex which stores real and imaginary part of a complex number.cpp
@@ -1,8 +1,3 @@
-/*Create a class complex which stores real and imaginary part of a complex number.
-Include all types of constructors and destructor. The destructor should display a
-message about the destructor being invoked. Create objects using different
-constructors and display them.*/
- 
 #include<iostream>
 using namespace std;
 
